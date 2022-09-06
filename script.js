c40036bdd768cb5a57c6cce79208a2b9bc9be0c1
ManchalaHarika <21r15a1201@gcet.edu.in>
2022-09-06 09:57:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function shopnow(){
+    alert("directinng you to your order")
+}
+function addtocart(){
+    alert("directinng you to your order")
+}
+function getdeal(){
+    alert("oder form kids toyzz")
+}
+var header =document.querySelector("a")
+header.style.color='blue'
+function getRandomColor()
+{
+    var letters ="0123456789ABCDEF";
+    var color="#";
+    for(var i=0;i<6;i++)
+    {
+        color +=letters[Math.floor(Math.random()*16)];
+    }
+    return color
+}
+function changeHeaderColor()
+{
+    colorInput =getRandomColor()
+    header.style.color = colorInput;
+}
+setInterval("changeHeaderColor()",500);
